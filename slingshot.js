25f84a0826af583100774a914724b25ed16c4ba5
Tiw4Coder <70450941+Tiw4Coder@users.noreply.github.com>
2020-11-29 13:20:17 -0500

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,22 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 200
+        }
+        this.sling = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.sling);
+    }
+    
+    display(){
+      var pointA = this.sling.bodyA.position
+      var pointB = this.pointB
+      strokeWeight(10)
+      line(pointA.x,pointA.y, pointB.x, pointB.y)
+      
+    }
+    
+}
